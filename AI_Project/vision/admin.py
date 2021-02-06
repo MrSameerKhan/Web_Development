541ed9bfbceda2ed28ed8c543414618ec28d5b72
Sameer Khan <sameerkhanofficial9@gmail.com>
2021-02-06 16:14:45 +0300

    Flast V1

--- AI_Project/vision/admin.py
+++ AI_Project/vision/admin.py
@@ -4,29 +4,3 @@ from .models import Poll, Choice
 
 admin.site.register(Poll)
 admin.site.register(Choice)
-"""
-
-
-chicken
-yusuf * 2
-arif
-kumar
-manoj
-venkat
-naimat(Regular)
-mukram
-bade
-
-
-fish
-sameer 
-shahid
-eliyaz
-
-
-broast
-wahed
-mahmood
-naimat
-
-"""
